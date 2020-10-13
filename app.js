e7ed4397530a3bf3be0b52198fd2a8dc59242852
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-10-13 07:36:22 +0530

    create directory

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+
+const port=8000;
+
+const app=express();
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in creating express server:${err}`);
+    }
+
+    console.log(`Server is running fine on port no.${port}`);
+});
