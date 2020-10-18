5b3b0cc3513fba9ca5c43339e23c9cdedf8eb00c
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-10-19 03:48:42 +0530

    creating controllers and routes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,19 @@ module.exports.profile=function(req,res){
         title:"Profile"
     });
 }
+
+// User Sign-In Page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"SignIn Page"
+    });
+};
+
+// User Sign-Up Page
+
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"SignUp Page"
+    });
+};
+
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,10 @@ const userController=require('../controllers/users_controller');
 
 router.get('/profile',userController.profile);
 
+router.get('/sign-in',userController.signIn);
+
+router.get('/sign-up',userController.signUp);
+
 
 
 module.exports=router;
