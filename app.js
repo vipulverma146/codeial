62604365bd5589acd5fe45749d35a5f2d0492b90
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-10-18 19:02:05 +0530

    setting up static files

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express=require('express');
 const port=8000;
 
 const app=express();
+// Using express-ejs-layouts
+const expressLayout=require('express-ejs-layouts');
+// extract style and script from sub-pages into layout
+
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
+//using assets folder
+app.use(express.static('./assets'));
+app.use(expressLayout);
 // use express router
 app.use('/',require('./routes/index'));
 
