252d038a406b8100e5b5c75bdc920e0b21792ed1
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-10-23 00:57:59 +0530

    Showing details of sign -in user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,22 @@
 const User=require('../models/user');
 
 module.exports.profile=function(req,res){
-    return res.render('user_profile',{
-        title:"Profile"
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){console.log("error",err); return}
+            if(user){
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user:user
+                });
+            }
+            return res.redirect('/users/sign-in');
+        })
+    }
+    else{
+        return res.redirect('/users/sign-in');
+
+    }
 }
 
 // User Sign-In Page
@@ -54,5 +67,24 @@ module.exports.create=function(req,res){
 // Sign-In and create session
 
 module.exports.createSession=function(req,res){
-    // Todo Later
+    // find the user
+    User.findOne({email:req.body.email},function(err,user){
+        // if user found
+        if(user){
+            // match the password with db password
+            if(req.body.password != user.password){
+                return res.redirect('back');
+            }
+            // handle session cookies
+            res.cookie('user_id',user.id);
+            return res.redirect('profile');
+        }
+
+        else{
+            // if user not found
+
+            return res.redirect('back');
+
+        }
+    })
 }
